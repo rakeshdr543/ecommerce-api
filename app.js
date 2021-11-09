ab4b4af8e1ac1dd978bf500e18053ea8dd013427
Rakesh D R <rakeshdr543@gmail.com>
2021-11-09 08:00:19 +0530

    Added basic setup

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+require('dotenv').config()
+require('express-async-errors')
 
+const express=require('express')
+
+const connectDB=require('./db/connect')
+
+// error handler
+const notFoundMiddleware=require('./middleware/not-found')
+const errorHandlerMiddleware=require('./middleware/error-handler')
+
+const app=express()
+
+app.use(express.json())
+
+// routes
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port=process.env.PORT || 3000
+
+const start=async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,()=>{
+            console.log(`Server is listening on port ${port} ...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
